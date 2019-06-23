87d8af40d9f563af88b4085353d53618afd3375a
unknown <pkell@DESKTOP-ED2GH22.(none)>
2019-06-23 11:34:43 -0700

    Dir Structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
